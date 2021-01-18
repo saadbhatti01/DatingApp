fb592492825ffdc9708e0c6f989ed156baf683ee
Saad Bhatti <saadbhatti001@outlook.com>
2021-01-18 20:50:37 +0500

    Enter

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,10 @@
 using API.Data;
 using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace API.Controllers
 {
@@ -11,6 +16,18 @@ namespace API.Controllers
         public UsersController(DataContext context)
         {
             this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await context.Users.FindAsync(id);
         }
     }
 }
